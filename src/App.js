4c062cee3afcb980784e1fe6110c7a09f6cbf352
mnatesannn <54511291+mnatesannn@users.noreply.github.com>
2022-11-27 18:22:14 -0500

    finished setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
+import Todo from './Todo';
+import NewTodo from './NewTodo';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Maya's ToDo App</h1>
+
+      <div className = "to-do-container">
+
+        <form id="to-do-form">
+
+            <div className="to-do-group" id = "list">
+
+              <NewTodo />
+              <Todo />
+
+            </div>
+
+        </form>
+
       </div>
+
     );
   }
 }
---
+++ src/Todo.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Todo.css';
+
+class Todo extends Component {
+  render() {
+    return (
+
+      <ul id="UL">
+
+      </ul>
+
+    );
+  }
+}
+
+export default Todo;
